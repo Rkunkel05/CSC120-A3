5fe106d7c2527351f9898ae633312e831b99a902
Rachel <rkunkel@smith.edu>
2024-02-09 10:03:30 -0500

    Started writing code. Input number of rounds and start convo.

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 class Conversation {
 
   public static void main(String[] arguments) {
     // You will start the conversation here.
+    Scanner input = new Scanner(System.in);
+    // Get user input for number of rounds
+    System.out.println("How many rounds of conversation do you want?: ");
+    int a = input.nextInt();
+    input.close();
+    // Start conversation
+    System.out.println("Hello! Welcome to the chatbot. What would you like to talk about?");
+    // Conversation continues while the user's request for conversation rounds is > 0
+    while (a>0); {
+      Scanner userInput = new Scanner(System.in);
+      String convo = userInput.nextLine();
+      System.out.println("Hm..."); // Won't print chatbot's response
+      a = a- 1; // Doesn't seem to actually change a...
+      userInput.close(); // Probably need to close this outside of the while loop...?
+    }
+
+
+    // make a for loop that runs for (user input) amount of rounds
+    // for (x rounds) {
+      // conversation loop
+      // user inputs something
+      // chatbot replies w/ something else (depends on punctuation, pronouns, etc.)
+      // exits loop when # of rounds met
+    } 
   }
-}
+
+  
+
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+* Huda
 - Which session(s) of TA / office hours did you attend?
+
 - What are your initial impressions of Java? 
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+* The Jav Docs (https://docs.oracle.com/en/java/javase/13/docs/api/index.html) are sooo useful!
