9317b2364f422f1ec65be67a2be9eb4fb28d9137
Rachel <rkunkel@smith.edu>
2024-02-10 14:57:15 -0500

    Set up 5 possible canned responses for chatbot to reply with randomly

--- Conversation.java
+++ Conversation.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 class Conversation {
 
@@ -8,17 +9,40 @@ class Conversation {
     // Get user input for number of rounds
     System.out.println("How many rounds of conversation do you want?: ");
     int a = input.nextInt();
-    input.close();
     // Start conversation
     System.out.println("Hello! Welcome to the chatbot. What would you like to talk about?");
     // Conversation continues while the user's request for conversation rounds is > 0
-    while (a>0); {
-      Scanner userInput = new Scanner(System.in);
+    Scanner userInput = new Scanner(System.in);
+    // Generates a random number for the response
+    int randomNum;
+                        // TO DO...
+                        // Check if pronouns in sentence
+                        // Mirror pronouns in response 
+                        // No pronouns? -> Canned response
+                        // Based on the number, selects a possibel response 
+    while (a>0) {
       String convo = userInput.nextLine();
-      System.out.println("Hm..."); // Won't print chatbot's response
+      randomNum = (int) (Math.random() * 5) + 1;
+      if (randomNum == 1) {
+        System.out.println("Hm...");
+      }
+      else if (randomNum == 2) {
+        System.out.println("Cool!");
+      }
+      else if (randomNum == 3) {
+        System.out.println("Interesting!");
+      }
+      else if (randomNum == 4) {
+        System.out.println("Fascinating...");
+      }
+      else if (randomNum == 4) {
+        System.out.println("Huh!");
+      }
       a = a- 1; // Doesn't seem to actually change a...
-      userInput.close(); // Probably need to close this outside of the while loop...?
     }
+    userInput.close();
+    input.close();
+    System.out.println("Thanks for chatting!");
 
 
     // make a for loop that runs for (user input) amount of rounds
--- rubric.md
+++ rubric.md
@@ -16,9 +16,9 @@ _____ 1 pt: `reflection.md` lists the classmates you consulted & TA hours attend
 
 **YES** 1 pt: Program queries user for number of conversation rounds
 
-_____ 1 pt: Program carries out requested number of conversation rounds
+**YES** 1 pt: Program carries out requested number of conversation rounds
 
-_____ 1 pt: Allocates static array of canned responses
+**YES** 1 pt: Allocates static array of canned responses
 
 ### Level 2 (3 pts)
 
